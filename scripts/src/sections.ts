56682a43d5091a84bcd4df80c45267788ff14699
anmedd-osama <ahmed.ossama10@gmail.com>
2023-04-26 06:56:24 +0200

    hero section finished

---
+++ scripts/src/sections.ts
@@ -0,0 +1,56 @@
+const heroSwiper = new Swiper('.hero-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 2000,
+  freeMode: true,
+  freeModeMomentum: false,
+  freeModeMomentumBounce: false,
+  autoplay: {
+    delay: 0 ,
+    disableOnInteraction: true,
+  },
+  slidesPerView: 'auto',
+  breakpoints: {
+    320: {
+        spaceBetween: 30
+    },
+    480: {
+        spaceBetween: 60
+    },
+    640: {
+        spaceBetween: 60
+    },
+    992: {
+        spaceBetween: 80
+    }
+}
+});
+const heroSwiperR = new Swiper('.hero-swiper-r', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  freeMode: true,
+  freeModeMomentum: false,
+  freeModeMomentumBounce: false,
+  speed: 2000,
+  autoplay: {
+    delay: 0 ,
+    disableOnInteraction: true,
+  },
+  slidesPerView: 'auto',
+  breakpoints: {
+    320: {
+        spaceBetween: 30
+    },
+    480: {
+        spaceBetween: 60
+    },
+    640: {
+        spaceBetween: 60
+    },
+    992: {
+        spaceBetween: 80
+    }
+}
+});
